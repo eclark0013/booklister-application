3d4d103ae6f320a1e6acf84b3e9a118c202a3ac9
Eric Clark <eclark0013@gmail.com>
2020-07-14 13:49:52 -0400

    Add resources

---
+++ booklister-backend/db/migrate/20200714174918_create_book_lists.rb
@@ -0,0 +1,11 @@
+class CreateBookLists < ActiveRecord::Migration[6.0]
+  def change
+    create_table :book_lists do |t|
+      t.integer :book_id
+      t.integer :list_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
