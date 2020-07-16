04cadf5def3383f45571ce6d2e2d5960373598b1
Eric Clark <eclark0013@gmail.com>
2020-07-16 15:42:47 -0400

    Fix bugs to start testing fetch.

--- booklister-backend/app/controllers/api/v1/users_controller.rb
+++ booklister-backend/app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,32 @@
 class Api::V1::UsersController < ApplicationController
+
+    def index
+        render json: User.all
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: user.errors
+        end
+    end
+
+    # def destroy
+    #     user = User.find(params[:id])
+    #     user.destroy
+    #     render json: User.all
+    # end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
 end
