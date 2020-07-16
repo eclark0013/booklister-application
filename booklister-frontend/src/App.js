214e695fcb2469d59a59540e5e95e9eae6411273
Eric Clark <eclark0013@gmail.com>
2020-07-16 16:05:14 -0400

    Test fetch in App.js

--- booklister-frontend/src/App.js
+++ booklister-frontend/src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  
+  state = {favoriteBook: ""}
+
+  componentDidMount(){
+    fetch('http://localhost:3000/api/v1/users/1/books')
+    .then(function(response){
+      return response.json()
+    })
+    .then(data => {
+      let favBook = data[0].title
+      this.setState({
+        favoriteBook: favBook
+      })
+      console.log(favBook)
+    })
+  }
+  
+
   render() {
     return (
       <div className="App">
-        <div>App</div>
+        <div>{this.state.favoriteBook}</div>
       </div>
     );
   }
