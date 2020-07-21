7d46936e53c83ef04a4e608fde06b75a46d27882
Eric Clark <eclark0013@gmail.com>
2020-07-21 14:38:35 -0400

    Set up redux dependencies on frontend and pry on backend

--- booklister-frontend/src/index.js
+++ booklister-frontend/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // import manageTodo from './reducers/manageTodo';
 // import { Provider } from 'react-redux';
-// import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import {thunk} from 'redux-thunk'
 
 ReactDOM.render(
   // <Provider>
